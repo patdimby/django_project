c4352badc5ba406c950b7cfffc849a3f520e651f
RANOELISON Dimbisoa Patrick <dimbisoapatrick@gmail.com>
2022-10-11 14:59:49 +0300

    update views post

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
@@ -11,3 +14,36 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
+
+@api_view(['GET', 'POST'])
+def post_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        posts_serializer = PostSerializer(posts, many=True)
+        return Response(posts_serializer.data)
+    elif request.method == 'POST':
+        post_serializer = PostSerializer(data=request.data)
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return Response(post_serializer.data, status=status.HTTP_201_CREATED)
+    return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+    try:
+        post = post.objects.get(pk=pk)
+    except post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        post_serializer = PostSerializer(post)
+        return Response(post_serializer.data)
+    elif request.method == 'PUT':
+        post_serializer = PostSerializer(post, data=request.data)
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return Response(post_serializer.data)
+        return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
