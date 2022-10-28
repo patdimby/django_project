5df6ffdaed2c293e5e7547ca4431137cd81ae4a3
RANOELISON Dimbisoa Patrick <dimbisoapatrick@gmail.com>
2022-10-28 12:43:18 +0300

    resume presentation

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+cleared[0] = cleared[1] = cleared[2] = 0; 
+function clearField(t) { 
+  if (!cleared[t.id]) { 
+    cleared[t.id] = 1; 
+    t.value = ''; 
+    t.style.color = '#fff';
+  }
+};
+
+const social = document.getElementById('socials')
+$.ajax({
+    type:'GET',
+    url : '/blog/socials',
+    success: function(response){          
+        const data = response.data            
+        data.forEach(element => {
+            socials.innerHTML +=`<li><a href="${element.link}">${element.title}</a></li>`
+        });
+    },
+    error: function(error){
+        console.log(error)
+    }
+});
